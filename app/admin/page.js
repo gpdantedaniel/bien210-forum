b4e8863eb269b6d70073bf4b30eb530d18c457eb
Dante Garcia <gp.dantedaniel@gmail.com>
2024-11-19 22:48:56 -0500

    Added counter

--- app/admin/page.js
+++ app/admin/page.js
@@ -8,6 +8,8 @@ export default function Home() {
   const [students, setStudents] = useState([]);
   const studentNames = students.map((student) => student.name);
 
+  console.log(students);
+
   useEffect(() => {
     const fetchQuestions = async () => {
       const { data, error } = await supabase
@@ -19,7 +21,10 @@ export default function Home() {
     };
 
     const fetchStudents = async () => {
-      const { data, error } = await supabase.from("students").select("*");
+      const { data, error } = await supabase
+        .from("students")
+        .select("*")
+        .order("name");
       if (!error) setStudents(data);
     };
 
@@ -48,6 +53,7 @@ export default function Home() {
           table: "students",
         },
         () => {
+          console.log("Fetching!");
           fetchStudents();
         }
       )
@@ -63,35 +69,48 @@ export default function Home() {
   }, [supabase]);
 
   const handleDeleteQuestion = async (id, name) => {
-    await supabase
-      .from("students")
-      .upsert({
-        name: name,
-      })
-      .select();
+    const data = students.filter((s) => s.name == name);
+
+    if (data.length == 0) {
+      await supabase
+        .from("students")
+        .upsert({ name: name, questions_answered: 1 })
+        .select();
+    } else {
+      const student_data = data[0];
+      const questions_answered = student_data.questions_answered;
+      await supabase
+        .from("students")
+        .update({ questions_answered: questions_answered + 1 })
+        .eq("name", name)
+        .select();
+    }
 
     await supabase.from("questions").delete().eq("id", id);
     setQuestions((prev) => prev.filter((q) => q.id !== id));
   };
 
   const deleteSession = async () => {
-    const { data: questions, error: error_questions } = await supabase
-      .from("questions")
-      .select("*");
-
-    const { data: students, error: error_students } = await supabase
-      .from("students")
-      .select("*");
-
+    const { data: questions } = await supabase.from("questions").select("*");
     const questions_ids = questions.map((q) => q.id);
-    const student_ids = students.map((s) => s.id);
-
-    await supabase.from("students").delete().in("id", student_ids);
     await supabase.from("questions").delete().in("id", questions_ids);
+
+    // const { data: students } = await supabase.from("students").select("*");
+    // const student_ids = students.map((s) => s.id);
+    // await supabase.from("students").delete().in("id", student_ids);
   };
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center gap-5">
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-5 p-2">
+      <h2 className="font-bold">Students who have asked questions</h2>
+      <div className="bg-gray-200 p-4 rounded-md min-w-0.5">
+        {students.map((s) => (
+          <div key={s.id} className="flex flex-row items justify-between gap-5">
+            <div>{s.name}</div>
+            <div>{s.questions_answered}</div>
+          </div>
+        ))}
+      </div>
       <h1 className="text-2xl font-bold">
         Admin Panel — Only for TAs and professors!
       </h1>
@@ -99,7 +118,7 @@ export default function Home() {
         className="px-3 py-1 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-300 transition ease-in-out duration-300"
         onClick={() => deleteSession()}
       >
-        Delete session (Careful!)
+        Clear Questions
       </button>
       <div className="flex flex-col gap-4">
         {questions.map((q) => (
@@ -110,9 +129,17 @@ export default function Home() {
             <div className="text-lg">{q.question}</div>
             <div className="text-sm">
               <strong>
-                {q.name}{" "}
+                {q.name}
                 {studentNames.includes(q.name) && (
-                  <span className="text-red-500"> ALREADY ANSWERED</span>
+                  <span className="text-red-500">
+                    {" "}
+                    ANSWERED{" "}
+                    {
+                      students.filter((s) => s.name == q.name)[0]
+                        .questions_answered
+                    }{" "}
+                    TIMES
+                  </span>
                 )}
               </strong>
             </div>
@@ -120,10 +147,10 @@ export default function Home() {
             <p>
               {
                 <button
-                  className="px-3 py-1 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-green-300 transition ease-in-out duration-300"
+                  className="px-3 py-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-green-300 transition ease-in-out duration-300"
                   onClick={() => handleDeleteQuestion(q.id, q.name)}
                 >
-                  Mark as answered
+                  Dismiss as answered
                 </button>
               }
             </p>
